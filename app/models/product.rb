07a73a0e7a4ebba464b0a4c9abe3ed6b62ff9516
Julian Bouma <bouma@post.harvard.edu>
2016-02-23 10:55:28 -0800

    quantity can be zero

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
   validates :name, :price, :description, :image_url, :quantity, presence: true
   validates_numericality_of :price, greater_than: 0
-  validates_numericality_of :quantity, greater_than: 0
+  validates_numericality_of :quantity, greater_than: -1
 end
