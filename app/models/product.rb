58d674fdbcb8bf460746796b67b486f2eefbfda9
baronkwan <baronkwan@outlook.com>
2016-03-23 11:48:43 -0700

    Add category model, categories migration and join table migration

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
 	validates :name, :description, :price, :quantity, presence: true
   	belongs_to :user
   	scope :sort_by_price, -> { order(:price) }
+    
+    has_and_belongs_to_many :categories
 end
