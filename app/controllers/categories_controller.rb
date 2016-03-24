acdbbeef86dacfe3a81fc4867177a1372d963c00
baronkwan <baronkwan@outlook.com>
2016-03-23 17:06:55 -0700

    Add some controller actions, views for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,45 @@
 class CategoriesController < ApplicationController
   def index
- 	@categories = Category.all
+   	@categories = Category.all
   end
 
   def new
+    if current_user.admin?
+      @category = Category.new
+    else
+      redirect_to '/'
+    end
   end
 
   def show
-  	@category = Category.find(params[:id])
-  	redirect_to category_products_path(@category)
+    @category = Category.find(params[:id])
+    redirect_to category_products_path(@category)
   end
+
+  def edit
+  	@category = Category.find(params[:id])
+    render 'edit'
+  end
+
+  def create
+    @category = Category.new(params[:id])
+    if @category.save
+      flash[:success] = "New category successfully created"
+      redirect_to "/admin"
+    else
+      flash[:error] = @category.error.full_messages.to_sentence
+      render 'new'
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(product_params)
+      flash[:success] = "Category successfully updated!"
+      redirect_to "/admin"
+    else
+      render 'edit'
+    end
+  end
+
 end
