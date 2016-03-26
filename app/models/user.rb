f8cddda9a152260ff6791a4b6388ecc235ca089e
Coline Forde <coline.forde@gmail.com>
2016-03-25 17:19:33 -0700

    add associations for orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable :trackable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :cart
-  after_create :create_cart
+  has_many :orders
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325180352) do
+ActiveRecord::Schema.define(version: 20160325235011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "carts", force: :cascade do |t|
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.decimal "total"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.boolean  "complete",    default: false
+    t.decimal  "grand_total"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "product_categories", force: :cascade do |t|
@@ -36,12 +45,10 @@ ActiveRecord::Schema.define(version: 20160325180352) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.integer  "reseller_id"
     t.string   "name"
     t.integer  "price"
     t.string   "description"
     t.integer  "quantity"
-    t.integer  "cart_id"
     t.string   "product_image"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
